3a7717b0cd5083ad2deddb381269eda3edac5139
Denys <dartist21@gmail.com>
2018-03-22 00:40:50 +0200

    feat: init vue-router

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const { asyncReadFile } = require('./helpers');
 
 const PORT = 3001;
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
+router.get('/', async (ctx, next) => {
   ctx.body = await asyncReadFile('./dist/index.html', 'utf8');
 });
+
+app.use(router.routes());
+// .use(router.allowedMethods());
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
